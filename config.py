cd6bf16d75764a49dc460e50d31d631ac2e9fd5e
Nir Wolff <wolffdesign@gmail.com>
2025-01-19 15:25:01 +0200

    Fixed database path and schema

--- config.py
+++ config.py
@@ -4,12 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = 'dev'
-    # Ensure instance folder exists
-    instance_path = os.path.join(basedir, 'instance')
-    if not os.path.exists(instance_path):
-        os.makedirs(instance_path)
-    
-    SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(instance_path, "app.db")}'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     # Basic SQLAlchemy settings
