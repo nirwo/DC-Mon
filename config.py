a50c22535587d07704d1df2f51b93d4146f3c465
Nir Wolff <wolffdesign@gmail.com>
2025-01-19 15:16:36 +0200

    Fixed database path

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
     SECRET_KEY = 'dev'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///shutdown_manager.db'
+    SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(basedir, "app.db")}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     # Basic SQLAlchemy settings
